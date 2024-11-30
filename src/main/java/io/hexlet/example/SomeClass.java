ce69aaf658bb9ee68716a1c8672540486930405e
Helgarold <mipakk95@gmail.com>
2024-11-30 14:02:00 +0300

    Creating the SomeClass class and a test for it

--- src/main/java/io/hexlet/example/SomeClass.java
+++ src/main/java/io/hexlet/example/SomeClass.java
@@ -1,7 +1,7 @@
 package io.hexlet.example;
 
-public class SomeClass {
-    public static int sum(int a, int b) {
-        return a + b;
-    }
-}
+   public class SomeClass {
+       public static int sum(int a, int b) {
+           return a + b;
+       }
+   }
